8719bc4b0af12bd50d2efac3231f0d5873306870
DmitriyKrasnikov <dimankrasnikov@mail.ru>
2025-08-03 14:20:58 +0300

    init commit

---
+++ src/main/java/com/krasnikov/kafkacloudbigdata/KafkaCloudBigDataApplication.java
@@ -0,0 +1,13 @@
+package com.krasnikov.kafkacloudbigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCloudBigDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCloudBigDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krasnikov/kafkacloudbigdata/KafkaCloudBigDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnikov.kafkacloudbigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCloudBigDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
